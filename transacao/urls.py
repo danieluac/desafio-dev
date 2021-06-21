e3be695051c458b0bbdb9511fbe062aa2dcea147
António Daniel <antoniocordeiro228@gmail.com>
2021-06-21 08:36:13 +0100

    rota para listagem de movimentos por loja

--- transacao/urls.py
+++ transacao/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-from .controllers.transacao_ctrl import TransacaoController
+from .controllers.transacao_ctrl import TransacaoController, lista_movimentosCtrl
 
 urlpatterns = [
     path('transacao/',TransacaoController.as_view()),
-    path('', RedirectView.as_view(url ='transacao/'))
+    path('', RedirectView.as_view(url ='transacao/')),
+    path('transacao/index', lista_movimentosCtrl)
 ]
